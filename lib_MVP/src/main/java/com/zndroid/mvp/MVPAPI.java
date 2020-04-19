36d7a56365487e8ca916aabe230fe27178a2356c
lazy <luzhenyuxfcy@sina.com>
2020-04-19 14:32:42 +0800

    create MVP demo project

---
+++ lib_MVP/src/main/java/com/zndroid/mvp/MVPAPI.java
@@ -0,0 +1,12 @@
+package com.zndroid.mvp;
+
+/**
+ * @name:MVPAPI
+ * @author:lazy
+ * @email:luzhenyuxfcy@sina.com
+ * @date : 2020/4/19 14:29
+ * @version:
+ * @description:
+ */
+public class MVPAPI {
+}
